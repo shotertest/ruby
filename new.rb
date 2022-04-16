8693df8540d04ca4358949c8808e352ef984a9de
EC2 Default User <ec2-user@ip-172-31-88-238.ec2.internal>
2022-04-16 13:04:40 +0000

    ruby 6

---
+++ new.rb
@@ -0,0 +1,16 @@
+class Car
+  def turn(direction)
+    puts "#{direction}に曲がります曲がります。"
+  end  
+  
+  
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+car = Car.new
+car.turn("右")
+
+car = Car.new
+car.run(5)
